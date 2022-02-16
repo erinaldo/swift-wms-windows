da745325b7bfa9247f777d0c05e4124842bba8d3
Jose <jose.l@procesoseficientes.com>
2022-02-16 14:23:40 -0600

    cambio en numero de version

--- WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
+++ WmsOnePlan/WMSOnePlan_Client/My Project/AssemblyInfo.vb
@@ -34,8 +34,8 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 
-<Assembly: AssemblyVersion("2022.02.07")>
-<Assembly: AssemblyFileVersion("2022.02.07")>
+<Assembly: AssemblyVersion("2022.02.16")>
+<Assembly: AssemblyFileVersion("2022.02.16")>
 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
